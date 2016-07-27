3a6321dacf44b4d3fc712209882c3a45c4842442
tuadmin <tuadmin@users.noreply.github.com>
2016-07-26 22:46:08 -0400

    change for compiler pyinstaller.py
    change for pyinstaller.py to exe file

--- yowsup/common/http/warequest.py
+++ yowsup/common/http/warequest.py
@@ -2,15 +2,18 @@ import urllib,sys, os, logging
 import hashlib
 from .waresponseparser import ResponseParser
 from yowsup.env import YowsupEnv
-
+#tuadmin add 6 ,7 line
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context    
 if sys.version_info < (3, 0):
     import httplib
     from urllib import urlencode
 
     if sys.version_info >= (2, 7, 9):
         #see https://github.com/tgalal/yowsup/issues/677
-        import ssl
-        ssl._create_default_https_context = ssl._create_unverified_context
+        #tuadmin change
+        #import ssl
+        #ssl._create_default_https_context = ssl._create_unverified_context
 
 else:
     from http import client as httplib
